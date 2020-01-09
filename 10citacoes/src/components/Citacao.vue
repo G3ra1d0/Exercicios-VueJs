658fab46cb938ee62be477504bb2f3dd68f1643d
G3ra1d0 <geraldo-vitor@hotmail.com>
2020-01-09 16:07:29 -0300

    Citações

---
+++ 10citacoes/src/components/Citacao.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="citacao">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        console.log('created')
+    }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
