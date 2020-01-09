788365fefe6cad35a6b4c35f88d8c2a8eca32fed
G3ra1d0 <geraldo-vitor@hotmail.com>
2020-01-09 15:26:22 -0300

    Componente Desafio

---
+++ 06Teste_Vue_CLI/src/contadores.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="contadores">
+        <app-contador v-for="c in 5" :key="c"/>
+    </div>
+</template>
+
+<script>
+import Contador from './contador.vue'
+export default {
+    components: { 'app-contador': Contador }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 07componente-desafio/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<v-app>
+		<app-menu />
+
+		<app-toobar />
+
+		<v-content>
+			<app-carrosel />
+	    </v-content>
+
+		<app-footer />
+	</v-app>
+</template>
+
+<script>
+import Footer from './footer.vue'
+import Carrosel from './carolsel.vue'
+import Toobar from './toobar.vue'
+import Menu from './menu.vue'
+
+export default {
+	components: {
+		'app-footer': Footer,
+		'app-carrosel': Carrosel,
+		'app-toobar': Toobar,
+		'app-menu': Menu
+	}
+}
+</script>
